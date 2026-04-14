4537738e130f338c4888ed6b53c5db697edbeec5
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2026-04-14 14:16:02 +0300

    Setup docker/docker compose, scripts and .env

---
+++ scripts/SetLocalEnvVariables.ps1
@@ -0,0 +1,4 @@
+# Need to insert local server name
+
+setx TEST_CONNECTION_STRING_13042026 "Server=#local server name#;Database=Test_13.04.2026;Trusted_Connection=True;TrustServerCertificate=True"
+
